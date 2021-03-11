9d8b0002a6d6842ab2c5e2f59ae8ec5a7c082511
kartikdua10 <80478828+kartikdua10@users.noreply.github.com>
2021-03-11 23:12:56 +0530

    Add files via upload

---
+++ TipCalculator/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace TipCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tipPercent = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numberOfPeople = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Bill = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tipPercent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPeople)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(29, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bill";
+            // 
+            // tipPercent
+            // 
+            this.tipPercent.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tipPercent.Location = new System.Drawing.Point(34, 115);
+            this.tipPercent.Name = "tipPercent";
+            this.tipPercent.ReadOnly = true;
+            this.tipPercent.Size = new System.Drawing.Size(269, 20);
+            this.tipPercent.TabIndex = 1;
+            this.tipPercent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(29, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tip %";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(29, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number of People";
+            // 
+            // numberOfPeople
+            // 
+            this.numberOfPeople.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.numberOfPeople.Location = new System.Drawing.Point(34, 201);
+            this.numberOfPeople.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numberOfPeople.Name = "numberOfPeople";
+            this.numberOfPeople.ReadOnly = true;
+            this.numberOfPeople.Size = new System.Drawing.Size(269, 20);
+            this.numberOfPeople.TabIndex = 2;
+            this.numberOfPeople.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numberOfPeople.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(381, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tip";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(386, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "per person";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(381, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Total";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label7.Location = new System.Drawing.Point(386, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "per person";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(533, 72);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 31);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Rs 00.00";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(533, 145);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(131, 31);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Rs 00.00";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(122, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Confirm";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Bill
+            // 
+            this.Bill.Location = new System.Drawing.Point(34, 38);
+            this.Bill.Name = "Bill";
+            this.Bill.ShortcutsEnabled = false;
+            this.Bill.Size = new System.Drawing.Size(269, 20);
+            this.Bill.TabIndex = 0;
+            this.Bill.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Bill.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Bill_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(752, 279);
+            this.Controls.Add(this.Bill);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numberOfPeople);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tipPercent);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tip Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.tipPercent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPeople)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown tipPercent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numberOfPeople;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Bill;
+    }
+}
+
---
+++ TipCalculator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // More than one decimal validation for field Bill
+            string[] test = Bill.Text.Split('.');
+            int decimalPos = Bill.Text.IndexOf('.');
+            string billDecimal = Bill.Text.Substring(decimalPos+1);
+            if (billDecimal.Contains('.'))
+            {
+                MessageBox.Show("Invalid Bill input");
+                Bill.Clear();
+                labelClear();
+            }
+
+            //decimal part greater than 3 validation for field bill
+            else if (test.Length == 2 && test[1].Length >= 3)
+            {
+                MessageBox.Show("Bill cannot have more than 2 decimal places");
+                Bill.Clear();
+                labelClear();
+            }
+
+            // Null or empty validation for field Bill
+            else if (!String.IsNullOrEmpty(Bill.Text) && Decimal.Parse(Bill.Text) != 0)
+            {
+                decimal billAmount = Decimal.Parse(Bill.Text);
+                decimal tipAmount = billAmount * (tipPercent.Value / 100);
+
+                decimal tipAmountPerPerson = Math.Round(tipAmount / numberOfPeople.Value, 2);
+                decimal totalBillPerPerson = Math.Round((billAmount + tipAmount) / numberOfPeople.Value,2);
+
+                label8.Text = "Rs " + tipAmountPerPerson.ToString();
+                label9.Text = "Rs " + totalBillPerPerson.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Bill cannot be empty or Zero.");
+                Bill.Clear();
+            }
+            
+        }
+
+        // method for clearing labels
+        private void labelClear()
+        {
+            label8.Text = "Rs 00.00";
+            label9.Text = "Rs 00.00";
+        }
+
+        // Non-numeric validation for the field Bill.
+        private void Bill_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && !e.KeyChar.Equals('.');
+        }
+    }
+}
